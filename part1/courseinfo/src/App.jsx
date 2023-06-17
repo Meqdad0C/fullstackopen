26fc3fc3ce35d33016dca110807514f0c44cbd1d
Meqdad Amr <Meqdad_@outlook.com>
2023-06-17 17:42:24 +0300

    part 1 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,33 @@
+/* eslint-disable react/prop-types */
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+    </>
+  )
+}
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.number}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -6,22 +36,17 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  const parts = [part1,part2,part3]
+  const exercisesArray = [exercises1,exercises2,exercises3]
+  
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercisesArray} />
+      <Total number={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
+
 
 export default App
