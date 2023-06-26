4f8ed6a55d396a6b7e6e02ae7212f8c1b04e2825
Meqdad Amr <Meqdad_@outlook.com>
2023-06-26 10:57:25 +0300

    part5 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,14 +1,120 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blog from './components/Blog.jsx'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import './App.css'
 
-const App = () => {
-  const [blogs, setBlogs] = useState([])
+const LoginForm = ({ setUser, setErrorMessage }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
-  }, [])
+  const handleLogin = async (event) => {
+    event.preventDefault()
 
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+          autoComplete={'off'}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+const BlogForm = ({ setBlogs, blogs }) => {
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
+
+  const handleBlogSubmit = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl,
+    }
+    const returnedBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(returnedBlog))
+
+    setNewBlogTitle('')
+    setNewBlogAuthor('')
+    setNewBlogUrl('')
+  }
+
+  return (
+    <>
+      <h2>blog form</h2>
+      <form onSubmit={handleBlogSubmit}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={newBlogTitle}
+            name="Title"
+            onChange={({ target }) => setNewBlogTitle(target.value)}
+            autoComplete={'off'}
+          />
+        </div>
+        <div>
+          Author:
+          <input
+            type="text"
+            value={newBlogAuthor}
+            name="Author"
+            onChange={({ target }) => setNewBlogAuthor(target.value)}
+            autoComplete={'off'}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={newBlogUrl}
+            name="Url"
+            onChange={({ target }) => setNewBlogUrl(target.value)}
+            autoComplete={'off'}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+}
+
+const DisplayBlogs = ({ blogs }) => {
   return (
     <div>
       <h2>blogs</h2>
@@ -16,6 +122,32 @@ const App = () => {
         <Blog key={blog.id} blog={blog} />
       ))}
     </div>
+  )
+}
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  return (
+    <>
+      {errorMessage && <div>{errorMessage}</div>}
+      {user === null && (
+        <LoginForm setUser={setUser} setErrorMessage={setErrorMessage} />
+      )}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          <DisplayBlogs blogs={blogs} />
+          {<BlogForm blogs={blogs} setBlogs={setBlogs} />}
+        </div>
+      )}
+    </>
   )
 }
 
