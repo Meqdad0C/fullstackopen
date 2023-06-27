ae5565527a279cb46f8a11343057d045a2521fc2
Meqdad Amr <Meqdad_@outlook.com>
2023-06-27 17:07:34 +0300

    part5 done

--- part5/bloglist-frontend/src/main.jsx
+++ part5/bloglist-frontend/src/main.jsx
@@ -1,15 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { StrictMode } from 'react'
-import { DevSupport } from '@react-buddy/ide-toolbox'
-import { ComponentPreviews, useInitial } from './dev/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
-      <App />
-    </DevSupport>
+    <App />
   </StrictMode>
 )
